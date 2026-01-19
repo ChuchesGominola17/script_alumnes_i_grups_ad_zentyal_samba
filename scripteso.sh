8b90351d483fb3c38a86bfe475e16696a3a1b19f
alumno <jaigomalb@alu.edu.gva.es>
2026-01-19 20:18:02 +0100

    Script con funciones 3_1eso 2eso 3eso 4eso_Jairo

--- scripteso.sh
+++ scripteso.sh
@@ -10,3 +10,22 @@ echo "1eso$letra"
 done
 }
 1eso
+2eso() {
+for letra in {a..d}; do
+echo "2eso$letra"
+done
+}
+2eso
+3eso() {
+for letra in {a..d}; do
+echo "3eso$letra"
+done
+}
+3eso
+4eso() {
+for letra in {a..d}; do
+echo "4eso$letra"
+done
+}
+4eso
+
